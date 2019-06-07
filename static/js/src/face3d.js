2540ab7b1761bd8f8b568d848f3c8086007a253b
Seita Uchimura <c201552k@gmail.com>
2019-06-07 02:43:36 -0700

    :watermelon: Add:three js でmask追加

--- static/js/src/face3d.js
+++ static/js/src/face3d.js
@@ -1,122 +1,63 @@
-let canvasWidth;
-let canvasHeight;
-let canvas;
-let shape;
-let video;
-let renderer;
-let pg;
-let context;
-const sketch = function(p5) {
-    p5.preload = async function() {
-        shape = p5.loadModel('models/mask.obj', true);
-    }
+window.addEventListener('load', init);
 
-    p5.setup = async function() {
-        const hero = document.getElementsByClassName("hero")[0];
-        canvas = craeateCanvasOfParentSize(p5, hero);
-        canvasWidth = hero.clientWidth;
-        canvasHeight = hero.clientHeight;
+function init() {
+    const scene = new THREE.Scene();
+    const hero = document.getElementsByClassName("hero")[0];
+    const width = hero.clientWidth;
+    const height = hero.clientHeight;
 
-        //p5.background(0, 255, 255);
-        //pg = p5.createGraphics(canvasWidth, canvasHeight, p5.WEBGL)
-        //pg = p5.createGraphics(100, 100, p5.WEBGL)
-        //pg.background(0, 255, 255);
-        //video = p5.createVideo(['videos/roller-coaster-short.mp4']);
-        //video.style('z-index', '4000');
-        //video.position(0, 0);
+    const onProgress = function(xhr) {
+        if (xhr.lengthComputable) {
+            var percentComplete = xhr.loaded / xhr.total * 100;
+            console.log(Math.round(percentComplete, 2) + '% downloaded');
+        }
+    };
+    const onError = function (xhr) { 
+        console.error("Error loading obj mtl: " + xhr);
+    };
+    let model;
+    const ObjLoader = new THREE.OBJLoader(); 
+    ObjLoader.load("models/mask.obj", function(obj) {
+        model = obj;
+        obj.scale.set(0.25, 0.25, 0.25);
+        obj.traverse(function(node) {
+            if(node.material) {
+                node.material.side = THREE.DoubleSide;
+            }
+        });
+        scene.add(obj);
+    }, onProgress, onError);
 
-        //video.loop().hide();
-        //pg = p5.createGraphics(canvasWidth, canvasHeight, p5.WEBGL);
-    }
+    //light
+    const light = new THREE.DirectionalLight("white", 1);
+    light.position.set(0, 0, 30);
+    light.castShadow = true;
+    scene.add(light);
 
-    p5.draw = async function() {
-        const canvasEl = document.getElementById(canvas.id());
-        context = canvasEl.getContext('webgl', { alpha: true });
-        //context.clearDepth(0.1);
-        context.colorMask(true, true, true, true);
-        context.clearColor(0, 0, 0, 0);
-        //p5.clear();
-        context.clear(context.DEPTH_BUFFER_BIT | context.COLOR_BUFFER_BIT);
-        //context.drawElements(context.TRIANGLES, 100, context.UNSIGNED_SHORT, 0);
+    //const ambient = new THREE.AmbientLight(0xffffff);
+    //scene.add(ambient);
 
-        //p5.push();
-        //p5.imageMode(p5.CENTER);
-        //p5.image(video, 0, 0);
-        //p5.pop();
+    const camera = new THREE.PerspectiveCamera(45, width/height, 1, 10000);
+    camera.position.set(0, 0, 1000);
 
-        //p5.image(pg, 0, 0);
+    const axis = new THREE.AxesHelper(2000);
+    axis.position.set(0, -1, 0);        
+    scene.add(axis);
 
-        //p5.push();
-        //p5.orbitControl();
-        //p5.lights();
-        //p5.rotateZ(p5.radians(180));
-        //p5.rotateY(p5.radians(-90));
-        //if(p5.mouseY < canvasHeight/2) {
-        //p5.rotateZ(p5.map(p5.mouseY, 0, canvasHeight/2, p5.HALF_PI/2, 0));
-        //} else {
-        //p5.rotateZ(p5.map(p5.mouseY, canvasHeight/2, canvasHeight, 0, -p5.HALF_PI/2));
-        //}
-        //p5.rotateY(p5.map(p5.mouseX, 0, canvasWidth, p5.HALF_PI, -p5.HALF_PI));
-        //p5.specularMaterial(250);
-        //p5.noStroke();
-        //p5.scale(2.5);
-        p5.model(shape);
-        //p5.pop();
-    }
+    const renderer = new THREE.WebGLRenderer({alpha: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(width, height);
+    renderer.setClearColor(0xeeeee, 0);    
+    renderer.shadowMap.enabled = true;       
 
-    p5.windowResized = function() {
-        const hero = document.getElementsByClassName("hero")[0];
-        canvasWidth = hero.clientWidth;
-        canvasHeight = hero.clientHeight;
-        p5.resizeCanvas(canvasWidth, canvasHeight);
-    }
+    hero.appendChild(renderer.domElement);
+    tick();
 
-    p5.mousePressed = function() {
-    }
-
-    p5.mouseWheel = function(event) {
+    function tick() {
+        renderer.render(scene, camera);
+        if(model != undefined) {
+            model.rotation.y += 0.01;
+        }
+        requestAnimationFrame(tick);
     }
 }
-
-function craeateCanvasOfParentSize(p5, parent) {
-    canvas = p5.createCanvas(parent.clientWidth, parent.clientHeight, p5.WEBGL);
-    //canvas = p5.createCanvas(parent.clientWidth, parent.clientHeight);
-    //canvas = p5.select('#canvas3d');
-    //canvas.parent(parent);
-    canvas.position(0, 0);
-    canvas.style('z-index', '3000');
-    const gl = start(canvas);
-    //canvas.style('opacity', '0.5');
-    //renderer = new THREE.WebGLRenderer({
-    //canvas: document.querySelector("#" + canvas.id())
-    //});
-    return canvas;
-}
-
-function start(glCanvas) {
-    const can = document.getElementById(glCanvas.id());
-    const gl = initWebGL(can);
-    if (gl) {
-        gl.clearColor(0.0, 0.0, 0.0, 0);
-        // 深度テストを有効化
-        gl.enable(gl.DEPTH_TEST);
-        gl.depthFunc(gl.LEQUAL);
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    }
-    return gl;
-}
-
-function initWebGL(can) {
-    let gl = null;
-    try {
-        gl = can.getContext("webgl") || canvas.getContext("experimental-webgl");
-    }
-    catch(e) {}
-    if (!gl) {
-        alert("WebGL を初期化できません。ブラウザはサポートしていないようです。");
-        gl = null;
-    }
-    return gl;
-}
-
-export let face3d = sketch;
