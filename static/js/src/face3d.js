0114b12dd03dcde199087cc4e0a36f8d01259ac1
Seita Uchimura <c201552k@gmail.com>
2019-06-08 21:05:31 -0700

    :watermelon: Add:masateruの顔を追加

--- static/js/src/face3d.js
+++ static/js/src/face3d.js
@@ -2,9 +2,9 @@ window.addEventListener('load', init);
 
 function init() {
     const scene = new THREE.Scene();
-    const hero = document.getElementsByClassName("hero")[0];
-    const width = hero.clientWidth;
-    const height = hero.clientHeight;
+    let hero = document.getElementsByClassName("hero")[0];
+    let width = hero.clientWidth;
+    let height = hero.clientHeight;
     let mouseX = width / 2;
     let mouseY = height / 2;
 
@@ -12,13 +12,37 @@ function init() {
         mouseX = event.clientX - window.innerWidth / 2;
         mouseY = event.clientY - window.innerHeight / 2;
         camera.position.x -= (mouseX + camera.position.x) * 0.1;
-        console.log("camera: " + camera.position.x);
-        console.log("mouseX: " + mouseX);
         camera.position.y += (mouseY - camera.position.y) * 0.2;
         camera.lookAt(scene.position);
     };
 
+    function onTouchMove(event) {
+        mouseX = getPositionX(event) - window.innerWidth / 2;
+        mouseY = getPositionY(event) - window.innerHeight / 2;
+        camera.position.x -= (mouseX + camera.position.x) * 0.1;
+        camera.position.y += (mouseY - camera.position.y) * 0.2;
+        camera.lookAt(scene.position);
+        function getPositionX(event) {
+            return event.touches[0].clientX;
+        }
+        function getPositionY(event) {
+            return event.touches[0].clientY;
+        }
+    }
+
+    function onResize() {
+        hero = document.getElementsByClassName("hero")[0];
+        width = hero.clientWidth;
+        height = hero.clientHeight;
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(width, height);
+
+        camera.aspect = width / height;
+        camera.updateProjectionMatrix();
+    }
+    window.addEventListener("resize", onResize, false);
     window.addEventListener("mousemove", onMouseMove, false);
+    window.addEventListener("touchmove", onTouchMove, false);
 
     const onProgress = function(xhr) {
         if (xhr.lengthComputable) {
@@ -30,25 +54,67 @@ function init() {
         console.error("Error loading obj mtl: " + xhr);
     }
     let model;
-    const ObjLoader = new THREE.OBJLoader(); 
-    ObjLoader.load("models/mask.obj", function(obj) {
-        obj.scale.set(0.25, 0.25, 0.25);
-        obj.rotation.y = -Math.PI / 2;
+    //const MtlLoader = new THREE.MTLLoader();
+    //MtlLoader.load(".mtl", function( materials ) {
+        //materials.preload();
+
+        //const ObjLoader = new THREE.OBJLoader(); 
+        ////ObjLoader.setMaterials( materials );
+        //ObjLoader.load("models/masateru.obj", function(obj) {
+            ////obj.scale.set(0.25, 0.25, 0.25);
+            //obj.scale.set(1.5, 1.5, 1.5);
+
+            //const iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+            //if(iOS) {
+                //obj.scale.set(0.15, 0.15, 0.15);
+            //}
+
+            //obj.rotation.y = -Math.PI / 2;
+            //obj.traverse(function(node) {
+                //if(node.material) {
+                    //node.material.side = THREE.DoubleSide;
+                //}
+            //});
+            //scene.add(obj);
+            //model = obj;
+        //}, onProgress, onError);
+    //});
+    
+    const onLoad = function(event) {
+        const obj = event.detail.loaderRootNode;
+        //obj.scale.set(0.25, 0.25, 0.25);
+        obj.scale.set(3, 3, 3);
+
+        const iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+        if(iOS) {
+            obj.scale.set(0.15, 0.15, 0.15);
+        }
+        console.log(obj.position.x);
+
+
+        //obj.rotation.y = -Math.PI / 2;
+        obj.rotation.z = Math.PI;
+        obj.position.x = 300;
+        obj.position.y = 300;
         obj.traverse(function(node) {
             if(node.material) {
                 node.material.side = THREE.DoubleSide;
             }
         });
-        model = obj;
         scene.add(obj);
-    }, onProgress, onError);
+        model = obj;
+    }
+    const ObjLoader = new THREE.OBJLoader2();
+    const filename = 'models/masateru2.obj';
+    //const filename = 'models/mask.obj';
+    ObjLoader.load(filename, onLoad, onProgress, onError, null, true);
 
     const light = new THREE.DirectionalLight("white", 1);
     light.position.set(0, 0, 30);
-    light.castShadow = true;
+    //light.castShadow = true;
     scene.add(light);
 
-    const camera = new THREE.PerspectiveCamera(45, width/height, 1, 10000);
+    let camera = new THREE.PerspectiveCamera(45, width/height, 1, 10000);
     camera.position.set(0, 0, 1000);
     camera.position.x -= (mouseX + camera.position.x) * 0.15;
     camera.position.y += (mouseY - camera.position.y) * 0.15;
@@ -64,17 +130,14 @@ function init() {
     renderer.setClearColor(0xeeeee, 0);    
     renderer.shadowMap.enabled = true;       
 
+    const controls = new THREE.OrbitControls(camera, renderer.domElement);
+
     hero.appendChild(renderer.domElement);
     tick();
 
-    let diffX = 0;
-    let diffY = 0;
     function tick() {
-        renderer.render(scene, camera);
+        controls.update();
         requestAnimationFrame(tick);
+        renderer.render(scene, camera);
     }
-
-    function map(n, start1, stop1, start2, stop2) {
-        return ((n-start1)/(stop1-start1))*(stop2-start2)+start2;
-    };
 }
